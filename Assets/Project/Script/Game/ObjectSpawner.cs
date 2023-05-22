0d0f4a5701f4ada391772ae453d840dd8b4bfcd3
MaxYnovobry <94967857+MaxYnovobry@users.noreply.github.com>
2023-05-22 11:46:24 +0200

    Hearth fix

--- Assets/Project/Script/Game/ObjectSpawner.cs
+++ Assets/Project/Script/Game/ObjectSpawner.cs
@@ -5,18 +5,15 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
-    [Header("Target variable")]
-    public GameObject prefab;
-    
-    [Header("Gameplay")]
-    public float interval;
+    [Header("Target variable")] public GameObject prefab;
+
+    [Header("Gameplay")] public float interval;
     public float Y;
-    
-    [Header("Visuals")]
-    public Sprite[] sprites;
+
+    [Header("Visuals")] public Sprite[] sprites;
 
     [Header("Sound")] public AudioSource slash_sound;
-    
+
     void Start()
     {
         InvokeRepeating("Spawns", interval, interval);
@@ -26,7 +23,7 @@ public class ObjectSpawner : MonoBehaviour
     {
         // Permet de créer un objet
         GameObject clone = Instantiate(prefab);
-        
+
         // Récupère la position X de l'objet de fond (Background)
         // Récupère la position X de l'objet de fond (Background)
         float minX = 400;
@@ -34,7 +31,7 @@ public class ObjectSpawner : MonoBehaviour
 
         // Génère une position X aléatoire entre les positions min et max de X du fond
         float randomX = Random.Range(minX, maxX);
-        
+
         // Permet de positionner l'objet
         clone.transform.position = new Vector3(
             randomX,
@@ -42,7 +39,7 @@ public class ObjectSpawner : MonoBehaviour
             260
         );
         clone.transform.SetParent(transform);
-            
+
         // Permet de changer le sprite de l'objet
         Sprite randomSprite = sprites[Random.Range(0, sprites.Length)];
         clone.GetComponent<SpriteRenderer>().sprite = randomSprite;
@@ -50,16 +47,17 @@ public class ObjectSpawner : MonoBehaviour
 
     public void HandleFruitCollision(GameObject fruit)
     {
-        // ...
-
-        if (collider2D.CompareTag("Bomb"))
+        if (fruit.CompareTag("Bomb"))
         {
             // Call the RemoveSprite() method on the Hearth script
-            GameObject hearthObject = GameObject.FindWithTag("Hearth");
-            if (hearthObject != null)
+            GameObject[] hearthObjects = GameObject.FindGameObjectsWithTag("Hearth");
+            foreach (GameObject hearthObject in hearthObjects)
             {
                 Hearth hearth = hearthObject.GetComponent<Hearth>();
-                hearth.RemoveSprite();
+                if (hearth != null)
+                {
+                    hearth.RemoveSprite();
+                }
             }
 
             // Add your desired logic here, such as increasing score, destroying the fruit, etc.
@@ -67,6 +65,14 @@ public class ObjectSpawner : MonoBehaviour
             fruit.GetComponent<FruitScript>().OnSliced();
             slash_sound.Play();
         }
-    }
+        else if (fruit.CompareTag("Fruit"))
+        {
+            // Add your desired logic here, such as increasing score, destroying the fruit, etc.
+            // For example, you can call a method on the fruit's script to handle its destruction:
+            fruit.GetComponent<FruitScript>().OnSliced();
+            slash_sound.Play();
+        }
 
+
+    }
 }
